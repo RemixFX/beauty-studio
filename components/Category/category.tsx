41778b14e380e2917e800cc42e3be9b443d18894
RemixFX <remix.master@yandex.ru>
2023-04-20 15:24:11 +0300

    refactor: change the method of using animation

--- components/Category/category.tsx
+++ components/Category/category.tsx
@@ -1,35 +1,36 @@
 'use client';
-import { ReactNode, useState } from 'react'
+import { ReactNode } from 'react'
 import styles from './category.module.scss'
-import { useTransition, animated } from '@react-spring/web'
+import { useSpring, useSpringRef, animated } from '@react-spring/web'
 import useMeasure from 'react-use-measure';
 
 export default function Category({ children }: { children?: ReactNode }) {
-  //const [ref, { height: viewHeight }] = useMeasure({offsetSize: true})
-  const [isActive, setActive] = useState(false)
-  const transitions = useTransition(isActive, {
-    from: {height: '0', transform: 'translate3d(0,-100%,0) scale(0.5)', opacity: 0 },
-    enter: {height: '570px', transform: 'translate3d(0,0%,0) scale(1)', opacity: 1 },
-    leave: {height: '0', transform: 'translate3d(0,-100%,0) scale(0.5)', opacity: 0 },
-    config: { duration: 430 }
+  const [ref, { height: viewHeight }] = useMeasure({ offsetSize: true })
+
+  const api = useSpringRef() 
+  const springs = useSpring({
+    ref: api,
+    from: { height: 0, opacity: 0, transform: 'translate3d(0,-100%,0) scale(0.5)' }
   })
 
-  const toggle = () => setActive(prev => !prev)
+  const handleClick = () => {
+    api.start({
+      to: {
+        opacity: springs.opacity.get() === 1 ? 0 : 1,
+        height: springs.height.get() === viewHeight ? 0 : viewHeight,
+        transform: springs.transform.get() === 'translate3d(0,0%,0) scale(1)' ? 'translate3d(0,-100%,0) scale(0.5)' : 'translate3d(0,0%,0) scale(1)'
+      },
+      config: { duration: 430 }
+    })
+  }
 
   return (
     <>
-      <button className={styles.button} onClick={toggle}>Пудровое напыление</button>
-      {
-        transitions((props, item) => {
-          return (
-            item &&
-            <animated.div style={props} className={styles.content} >
-              {children}
-            </animated.div>
-          )
-        })
-      }
-
+      <button className={styles.button} onClick={handleClick}>Пудровое напыление</button>
+      <animated.div style={springs} className={styles.content} >
+        <div ref={ref}>{children}</div>
+      </animated.div>
     </>
+
   )
 }
