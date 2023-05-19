e36f5e03f2562258dc144aa61a94016924013012
RemixFX <remix.master@yandex.ru>
2023-05-19 12:56:20 +0300

    feat: add navigation bar to the enlist page

--- pages/enlist.tsx
+++ pages/enlist.tsx
@@ -3,31 +3,35 @@ import styles from '@/styles/enlist.module.scss'
 import Link from "next/link";
 import Footer from "@/components/Footer/footer";
 import Layout from "@/components/Layout/layout";
+import NavPanel from "@/components/NavPanel/nav-panel";
 
 export default function Enlist() {
   return (
     <><Head>
       <title>Онлайн запись</title>
     </Head>
+    <div className={styles.navigation}>
+      <NavPanel />
+    </div>
       <Layout>
-      <section className={styles.content}>
-      <h1 className={styles.header}>Запись</h1>
-        <p className={styles.description}>есть несколько способов записаться:</p>
-        <Link href={'#'} className={`${styles.button} ${styles.button_calendar}`}>Онлайн</Link>
-        <a href="https://wa.me/+79883670897?text=Здравствуйте!%20Хочу%20записаться%20на%20процедуру"
-          className={`${styles.button} ${styles.button_whatsapp}`}>Через Whatsapp</a>
-        <a href="https://t.me/ilonkaizmaylova"
-          className={`${styles.button} ${styles.button_telegram}`}>Через Telegram</a>
-        <p className={styles.description}>или попросить консультацию:</p>
-        <a href="#" className={`${styles.button} ${styles.button_phone}`}>Оставить телефон</a>
-        <a href="https://wa.me/+79883670897?text=Здравствуйте!%20Хочу%20проконсультироваться"
-          className={`${styles.button} ${styles.button_whatsapp}`}>Написать в Whatsapp</a>
-        <p className={styles.description}>посмотреть доступные даты:</p>
-        <Link href={'#'} className={`${styles.button} ${styles.button_calendar}`}>В календаре</Link>
-      </section>
+        <section className={styles.content}>
+          <h1 className={styles.header}>Запись</h1>
+          <p className={styles.description}>есть несколько способов записаться:</p>
+          <Link href={'#'} className={`${styles.button} ${styles.button_calendar}`}>Онлайн</Link>
+          <a href="https://wa.me/+79883670897?text=Здравствуйте!%20Хочу%20записаться%20на%20процедуру"
+            className={`${styles.button} ${styles.button_whatsapp}`}>Через Whatsapp</a>
+          <a href="https://t.me/ilonkaizmaylova"
+            className={`${styles.button} ${styles.button_telegram}`}>Через Telegram</a>
+          <p className={styles.description}>или попросить консультацию:</p>
+          <a href="#" className={`${styles.button} ${styles.button_phone}`}>Оставить телефон</a>
+          <a href="https://wa.me/+79883670897?text=Здравствуйте!%20Хочу%20проконсультироваться"
+            className={`${styles.button} ${styles.button_whatsapp}`}>Написать в Whatsapp</a>
+          <p className={styles.description}>посмотреть доступные даты:</p>
+          <Link href={'#'} className={`${styles.button} ${styles.button_calendar}`}>В календаре</Link>
+        </section>
       </Layout>
       <Footer />
-      
+
     </>
   )
 }
