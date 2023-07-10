8e3474c7d9fcac43cf03a0b5424677597dc040f1
RemixFX <remix.master@yandex.ru>
2023-07-10 14:48:14 +0300

    style: visual effect for current route nav-link, hover

--- components/desktop/NavPanelDesktop/nav-panel-desktop.tsx
+++ components/desktop/NavPanelDesktop/nav-panel-desktop.tsx
@@ -1,15 +1,22 @@
 import styles from './nav-panel-desktop.module.scss'
 import Link from 'next/link'
+import { useRouter } from "next/router";
 import { LINKS } from '@/config/NavPanel.config'
 
-export default function NavPanelDesktop () {
+export default function NavPanelDesktop() {
 
   const LinksDesktop = LINKS.filter(element => element.name !== 'Записаться')
-  
-  return(
+  const { pathname, asPath } = useRouter()
+
+  console.log(pathname.slice(- asPath.length), pathname, asPath)
+
+  return (
     <nav className={styles.navigation}>
-      {LinksDesktop.map((link) => 
-        <Link href={link.src} key={link.name} className={styles.link}>{link.name}</Link>
+      {LinksDesktop.map((link) =>
+        <Link href={link.src} key={link.name}
+          className={`${styles.link} ${link.src === asPath || link.src === pathname ? styles.link_active : ''}`}>
+          {link.name}
+        </Link>
       )}
     </nav>
   )
