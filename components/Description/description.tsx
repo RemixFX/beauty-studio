5f089280074fc5109b001d408aca8c59d98a1fb6
RemixFX <remix.master@yandex.ru>
2023-07-14 16:12:53 +0300

    style: adaptive for the first block of the main page

--- components/Description/description.tsx
+++ components/Description/description.tsx
@@ -1,33 +1,61 @@
 import Image from 'next/image'
 import styles from './description.module.scss'
 import logo from '@/public/lib/logo/logo_type2.png'
+import master from '@/public/lib/home/master1.png'
 
 export default function Description() {
   return (
     <section className={styles.section} id='services'>
-      <h1 className={styles.title}>Студия татуажа<br />Илоны Измайловой</h1>
-      <blockquote className={styles.quote}>&#34;Каждая девушка достойна...
-        <span className={styles.quote__parth}> ...быть красивой&#34;</span>
-      </blockquote>
-        <Image src={logo} alt="студия перманентного татуажа" className={styles.logo}/>
-      <p className={styles.about}>что мы можем сделать для Вас:</p>
-      <ul className={styles.description}>
-        <li className={styles.service}>
-          <a href='#brows' className={styles.link}>Татуаж бровей</a>
-        </li>
-        <li className={styles.service}>
-          <a href='#lips' className={styles.link}>Татуаж губ</a>
-        </li>
-        <li className={styles.service}>
-          <a href='#eyelashes' className={styles.link}>Татуаж век</a>
-        </li>
-        <li className={styles.service}>
-          <a href='#correction' className={styles.link}>Коррекция татуажа</a>
-        </li>
-        <li className={styles.service}>
-          <a href='#overlap' className={styles.link}>Перекрытие татуажа</a>
-        </li>
-      </ul>
+      <div className={styles.left_block_mobile}>
+        <h1 className={styles.title}>Студия татуажа<br />Илоны Измайловой</h1>
+        <blockquote className={styles.quote}>&#34;Каждая девушка достойна...
+          <span className={styles.quote__parth}> ...быть красивой&#34;</span>
+        </blockquote>
+        <ul className={styles.description_mobile}>
+          <li className={styles.element}>
+            <span className={styles.mnemonic}>&#10003; </span>
+            Сертифицированный мастер перманентного макияжа
+            </li>
+          <li className={styles.element}>
+          <span className={styles.mnemonic}>&#10003; </span>
+            Гарантия качественного и стойкого результата
+            </li>
+          <li className={styles.element}>
+          <span className={styles.mnemonic}>&#10003; </span>
+            5 лет практики и 3000 довольных клиентов
+            </li>
+          <li className={styles.element}>
+          <span className={styles.mnemonic}>&#10003; </span>
+            Сертифицированные пигменты класса Люкс
+          </li>
+        </ul>
+      </div>
+      <div className={styles.right_block_mobile}>
+        <Image src={master} alt="мастер перманентного татуажа" className={styles.master} />
+        <div className={styles.image_layout}>
+        <Image src={logo} alt="студия перманентного татуажа" className={styles.logo} />
+        </div>
+      </div>
+      <div className={styles.bottom_block_mobile}>
+        <p className={styles.about}>что мы можем сделать для Вас:</p>
+        <ul className={styles.services}>
+          <li className={styles.service}>
+            <a href='#brows' className={styles.link}>Татуаж бровей</a>
+          </li>
+          <li className={styles.service}>
+            <a href='#lips' className={styles.link}>Татуаж губ</a>
+          </li>
+          <li className={styles.service}>
+            <a href='#eyelashes' className={styles.link}>Татуаж век</a>
+          </li>
+          <li className={styles.service}>
+            <a href='#correction' className={styles.link}>Коррекция татуажа</a>
+          </li>
+          <li className={styles.service}>
+            <a href='#overlap' className={styles.link}>Перекрытие татуажа</a>
+          </li>
+        </ul>
+      </div>
     </section>
   )
 }
